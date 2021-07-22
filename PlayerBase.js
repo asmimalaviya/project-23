2929884f1d9f4241e1e20bd1816146c8c6f2286f
asmimalaviya <86877268+asmimalaviya@users.noreply.github.com>
2021-07-22 13:51:52 +0530

    Add files via upload

---
+++ PlayerBase.js
@@ -0,0 +1,29 @@
+class PlayerBase {
+constructor(x,y,width,height)
+var option= {
+isStatic : true
+};
+this. body=Bodies.rectangle(x,y,width,height,option);
+this. width=width;
+this.height=height;
+this.image= loadImage("./assets/base1.png");
+
+
+World.add(world,this.body)
+}
+
+display() {
+    var pos = this.body.position;
+var angel = this.body.angel;
+
+push();
+translate(pos.x,pos.y);
+
+rotate(angel)
+imageMode(CENTER)
+image(this.image,0,0,this. width,this.height);
+pop();
+
+
+}
+}
